94e3bcec20d658056a4984f4a8f4b4fd4eef713b
Franion <franionyt@gmail.com>
2022-11-20 14:50:18 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>subskrybuj macioch DEV</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
